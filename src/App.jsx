3ec6d03ffbf7ae052a98213903eda2b18464c41a
Magaly Puente Parada <magapp92@alumnos.cei.es>
2026-06-17 23:59:53 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Magos } from './components/Magos'
+
+const App = () => {
+    
+    return (
+        <>
+            <Magos />
+        </>
+    )
+}
+
+export default App
